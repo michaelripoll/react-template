e2934cfec6c7a740bd897a18dd926b4ca01de26f
Michael Ripoll <michael-ripoll@hotmail.com>
2022-05-16 23:28:07 -0500

    Se añade navbar y sidebar sin estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.scss';
+import Navbar from './components/NavBar';
+import Admin from './components/pages/Admin';
+import Home from './components/pages/Home';
+import Movement from './components/pages/Movement';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      Hola mundo
-    </div>
+      <BrowserRouter>
+        <Navbar/>
+          <div className='flex'>
+            <Sidebar/>
+            <div className='content'>
+              <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/admin" element={<Admin/>}/>
+                <Route path="/movement" element={<Movement/>}/>
+              </Routes>
+            </div>
+          </div>
+      </BrowserRouter>
   );
 }
 
---
+++ src/components/pages/Movement.tsx
@@ -0,0 +1,9 @@
+const Movement = () => {
+    return(
+        <div>
+            <p>Movement</p>
+        </div>
+    )
+}
+
+export default Movement;
